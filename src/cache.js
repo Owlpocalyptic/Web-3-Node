01de6913ee0d01c4c597ca9754827565e5bf8aa7
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-06-07 10:56:35 +1200

    Added Country display page

--- src/cache.js
+++ src/cache.js
@@ -1,10 +1,14 @@
+let values;
+
 if (localStorage.getItem('countries'))
 {
-
+    console.log("Local Storage: " + localStorage.getItem('countries'));
+    values = localStorage.getItem('countries');
 }
 else
 {
     $.get("10.25.137.196:5000/api/countries", (response) => {
         console.log("Response: " + response);
+        localStorage.setItem('countries', 'response');
     });
 }
