9a838c02cbb4b02af42fd23cb4965e6e05b1d9a7
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-06-07 11:05:34 +1200

    Updated country display page

--- src/cache.js
+++ src/cache.js
@@ -2,13 +2,13 @@ let values;
 console.log("Cache.js is running");
 if (localStorage.getItem('countries') === null)
 {
-    console.log("Local Storage: " + localStorage.getItem('countries'));
-    values = localStorage.getItem('countries');
-}
-else
-{
     $.get("10.25.137.196:80/api/countries", (response) => {
         console.log("Response: " + response);
         localStorage.setItem('countries', 'response');
     });
+}
+else
+{
+    console.log("Local Storage: " + localStorage.getItem('countries'));
+    values = localStorage.getItem('countries');
 }
