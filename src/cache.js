c697762bdd83a46dbb573fcb28c413719eea40b6
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-06-07 12:07:18 +1200

    Updated country display page

--- src/cache.js
+++ src/cache.js
@@ -2,7 +2,7 @@ let values;
 console.log("Cache.js is running");
 if (localStorage.getItem('countries') === null)
 {
-    $.get("10.25.137.196/api/countries", (response) => {
+    $.get("10.25.137.196:80/api/countries", (response) => {
         console.log("Response: " + response);
         localStorage.setItem('countries', 'response');
     });
