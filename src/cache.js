9008ad8f500e492434a57c2b0843a992bced7a9b
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-06-14 10:45:54 +1200

    Testing API route

--- src/cache.js
+++ src/cache.js
@@ -1,5 +1,14 @@
 let values;
 console.log("Cache.js is running");
+
+$.ajaxSetup({
+    type: "GET",
+    data: {},
+    dataType: 'json',
+    crossDomain: true,
+    contentType: 'application/json; charset=utf-8'
+});
+
 if (localStorage.getItem('countries') === null)
 {
     $.get("10.25.137.196:80/api/test", (response) => {
