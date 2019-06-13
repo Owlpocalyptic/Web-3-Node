cb00e484b1909f7b43025a3415f28c9ecb59861e
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-06-14 11:33:18 +1200

    Testing API route

--- src/cache.js
+++ src/cache.js
@@ -5,7 +5,7 @@ console.log("Cache.js is running");
 if (localStorage.getItem('countries') === null)
 {
     console.log("Well, the checking part worked.")
-    fetch("10.25.137.137:80/api/countries", { method: 'get', mode: 'CORS'}, (response) => {
+    fetch("10.25.137.137:80/api/countries", { method: 'get', mode: 'cors'}, (response) => {
         console.log("Response: " + response);
         localStorage.setItem('countries', 'response');
     });
