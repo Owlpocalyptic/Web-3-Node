08880623c7448c03f24018db33728e0b200bfc55
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-06-14 11:28:39 +1200

    Testing API route

--- src/cache.js
+++ src/cache.js
@@ -4,6 +4,7 @@ console.log("Cache.js is running");
 
 if (localStorage.getItem('countries') === null)
 {
+    console.log("Well, the checking part worked.")
     $.get("10.25.137.137:80/api/countries", (response) => {
         console.log("Response: " + response);
         localStorage.setItem('countries', 'response');
