fc73cc834938829a89261daf4789cf93fd20f95d
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-06-19 09:56:05 +1200

    Managed to read JSON objects. Working on doing stuff with them now.

--- public/javascript/cache.js
+++ public/javascript/cache.js
@@ -1,5 +1,39 @@
-if (localStorage.getItem('countries')) {} else {
-    $.get("127.0.0.1:5000/api/countries", function (response) {
-        console.log("Response: " + response);
+var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol" ? function (obj) { return typeof obj; } : function (obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; };
+
+var values = void 0;
+console.log("Cache.js is running");
+
+var loadList = function loadList(items) {
+    itemsJSON = JSON.parse(items);
+    console.log(typeof itemsJSON === "undefined" ? "undefined" : _typeof(itemsJSON));
+    var selectbox = $("#country-select");
+    for (var i = 0; i < itemsJSON.length; i++) {
+        var country = itemsJSON[i]["name"];
+        selectbox.append("<option value='" + country + "'>" + country + "</option>");
+        console.log(itemsJSON[i]["name"]);
+    }
+    selectbox.addEventListener("change", function (event) {
+        var infotitle = $("#title");
+        infotitle.append("SELECTED");
     });
+};
+
+if (localStorage.getItem('countries') === null) {
+    console.log("Well, the checking part worked.");
+    $.get("http://10.25.137.137:80/api/countries", function (response) {
+        responseJSON = JSON.parse(response);
+        console.log("Response: " + responseJSON);
+        //localStorage.setItem('countries', JSON.stringify(responseJSON));
+        values = responseJSON;
+        return values;
+        //console.log(JSON.stringify(values));
+    }).then(function (values) {
+        loadList(values);
+    });
+} else {
+    values = localStorage.getItem('countries');
+    console.log("Local Storage: " + values);
+    valuesString = JSON.stringify(values);
+    console.log(JSON.parse(valuesString));
+    loadList(JSON.parse(valuesString));
 }
--- src/cache.js
+++ src/cache.js
@@ -1,18 +1,42 @@
 let values;
 console.log("Cache.js is running");
 
+const loadList = (items) => {
+    itemsJSON = JSON.parse(items);
+    console.log(typeof itemsJSON);
+    let selectbox = $("#country-select");
+    for (let i=0;i< itemsJSON.length;i++)
+    {
+        let country = itemsJSON[i]["name"];
+        selectbox.append("<option value='" + country + "'>" + country + "</option>");
+        console.log(itemsJSON[i]["name"]);
+    }
+    selectbox.addEventListener("change", (event) => {
+        let infotitle = $("#title");
+        infotitle.append("SELECTED");
+    });
+}
+
 
 if (localStorage.getItem('countries') === null)
 {
     console.log("Well, the checking part worked.")
-    fetch("http://10.25.137.137:80/api/countries", { method: 'get', mode: 'cors'})
-        .then((response) => {
-        console.log("Response: " + response);
-        localStorage.setItem('countries', 'response');
+    $.get("http://10.25.137.137:80/api/countries", (response) => {
+        responseJSON = JSON.parse(response);
+        console.log("Response: " + responseJSON);
+        //localStorage.setItem('countries', JSON.stringify(responseJSON));
+        values = responseJSON;
+        return values;
+        //console.log(JSON.stringify(values));
+    }).then((values) => {
+        loadList(values);
     });
 }
 else
 {
-    console.log("Local Storage: " + localStorage.getItem('countries'));
     values = localStorage.getItem('countries');
+    console.log("Local Storage: " + values);
+    valuesString = JSON.stringify(values);
+    console.log(JSON.parse(valuesString));
+    loadList(JSON.parse(valuesString));
 }
