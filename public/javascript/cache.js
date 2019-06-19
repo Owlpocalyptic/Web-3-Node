fab9704070e586b8d4a4d65ebc8f2498e5fdd212
mulrnk1 <mulrnk1@student.op.ac.nz>
2019-06-19 14:01:09 +1200

    Now displays all information, with graceful N/As when that information doesn't exist!

--- public/javascript/cache.js
+++ public/javascript/cache.js
@@ -13,14 +13,21 @@ var loadList = function loadList(items) {
     }
     selectbox.change(function (event) {
         var itemJSON = itemsJSON[event.target.value];
+        var year = "2005";
         var infotitle = $("#title");
         var infoPopulation = $("#population");
+        var infoEmployment = $("#employment-rate");
+        var infoSpending = $("#spending");
+        var infoAlcohol = $("#alcohol-consumption");
+        var infoInternet = $("#internet-access");
         infotitle.text(itemJSON["name"]);
-        infoPopulation.text(function () {
-            var popData = itemJSON["data"]["population"]["2005"];
-            var popString = itemJSON["data"]["population"] !== undefined ? popData : "N/A";
-            return "Population: " + popString;
-        });
+        infoPopulation.text(getFromJSON("population", year, itemJSON));
+        var suffix = getFromJSON("employment", year, itemJSON) === "N/A" ? "" : "%";
+        infoEmployment.text(getFromJSON("employment", year, itemJSON, "Rate", "", suffix));
+        var prefix = getFromJSON("spending", year, itemJSON) === "N/A" ? "" : "$";
+        infoSpending.text(getFromJSON("spending", year, itemJSON, "Per Capita", prefix));
+        infoAlcohol.text(getFromJSON("alcohol", year, itemJSON, "Consumption"));
+        infoInternet.text(getFromJSON("internet", year, itemJSON, "Access"));
     });
 };
 
@@ -39,3 +46,43 @@ if (localStorage.getItem('countries') === null) {
     valuesString = JSON.stringify(values);
     loadList(JSON.parse(valuesString));
 }
+
+var getFromJSON = function getFromJSON(field, year, itemJSON) {
+    var extra = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : "";
+    var prefix = arguments.length > 4 && arguments[4] !== undefined ? arguments[4] : "";
+    var suffix = arguments.length > 5 && arguments[5] !== undefined ? arguments[5] : "";
+
+    var rawData = itemJSON["data"][field] !== undefined ? itemJSON["data"][field][year] : undefined;
+    console.log(rawData);
+    var numString = rawData !== undefined && rawData != 0 ? prefix + formatNumbers(rawData) + suffix : "N/A";
+    extra = extra != "" ? " " + extra : "";
+    return capitalize(field) + extra + ": " + numString;
+};
+
+var formatNumbers = function formatNumbers(amount) {
+    var decimalCount = arguments.length > 1 && arguments[1] !== undefined ? arguments[1] : 0;
+    var decimal = arguments.length > 2 && arguments[2] !== undefined ? arguments[2] : ".";
+    var thousands = arguments.length > 3 && arguments[3] !== undefined ? arguments[3] : ",";
+
+    try {
+        decimalCount = Math.abs(decimalCount);
+        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+        var negativeSign = amount < 0 ? "-" : "";
+
+        var i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+        var j = i.length > 3 ? i.length % 3 : 0;
+
+        return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+    } catch (e) {
+        console.log(e);
+    }
+};
+
+var capitalize = function capitalize(s) {
+    if (typeof s !== 'string') return '';
+    n = s.replace("_", " ");
+    n = n.replace("id", "ID");
+    n = n.charAt(0).toUpperCase() + n.slice(1);
+    return n;
+};
--- src/cache.js
+++ src/cache.js
@@ -12,14 +12,21 @@ const loadList = (items) => {
     }
     selectbox.change((event) => {
         const itemJSON = itemsJSON[event.target.value];
+        const year = "2005"
         let infotitle = $("#title");
-        let infoPopulation=$("#population")
+        let infoPopulation=$("#population");
+        let infoEmployment=$("#employment-rate");
+        let infoSpending=$("#spending");
+        let infoAlcohol=$("#alcohol-consumption");
+        let infoInternet=$("#internet-access");
         infotitle.text(itemJSON["name"]);
-        infoPopulation.text(() => {
-            const popData = itemJSON["data"]["population"]["2005"]
-            const popString = (itemJSON["data"]["population"] !== undefined) ? popData : "N/A";
-            return "Population: " + popString;
-        });
+        infoPopulation.text(getFromJSON("population", year, itemJSON));
+        let suffix = (getFromJSON("employment", year, itemJSON) === "N/A") ? "" : "%";
+        infoEmployment.text(getFromJSON("employment", year, itemJSON, "Rate", "", suffix));
+        let prefix = (getFromJSON("spending", year, itemJSON) === "N/A") ? "" : "$";
+        infoSpending.text(getFromJSON("spending", year, itemJSON, "Per Capita", prefix));
+        infoAlcohol.text(getFromJSON("alcohol", year, itemJSON, "Consumption"));
+        infoInternet.text(getFromJSON("internet", year, itemJSON, "Access"));
     });
 }
 
@@ -42,3 +49,35 @@ else
     valuesString = JSON.stringify(values);
     loadList(JSON.parse(valuesString));
 }
+
+const getFromJSON = (field, year, itemJSON, extra = "", prefix = "", suffix = "") => {
+    const rawData = (itemJSON["data"][field] !== undefined) ? itemJSON["data"][field][year] : undefined;
+    console.log(rawData);
+    const numString = (rawData !== undefined && rawData != 0) ? prefix + formatNumbers(rawData) + suffix : "N/A";
+    extra = (extra != "") ? " " + extra : "";
+    return capitalize(field) + extra + ": " + numString;
+};
+
+const formatNumbers = (amount, decimalCount = 0, decimal = ".", thousands = ",") => {
+    try {
+        decimalCount = Math.abs(decimalCount);
+        decimalCount = isNaN(decimalCount) ? 2 : decimalCount;
+
+        const negativeSign = amount < 0 ? "-" : "";
+
+        let i = parseInt(amount = Math.abs(Number(amount) || 0).toFixed(decimalCount)).toString();
+        let j = (i.length > 3) ? i.length % 3 : 0;
+
+        return negativeSign + (j ? i.substr(0, j) + thousands : '') + i.substr(j).replace(/(\d{3})(?=\d)/g, "$1" + thousands) + (decimalCount ? decimal + Math.abs(amount - i).toFixed(decimalCount).slice(2) : "");
+    } catch (e) {
+        console.log(e)
+    }
+};
+
+const capitalize = (s) => {
+    if (typeof s !== 'string') return '';
+    n = s.replace("_", " ");
+    n = n.replace("id", "ID");
+    n = n.charAt(0).toUpperCase() + n.slice(1);
+    return n;
+};
